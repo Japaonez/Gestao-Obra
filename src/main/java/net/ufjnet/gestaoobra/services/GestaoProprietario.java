ecb8077c7b750392a04525f8a382656767838802
Japaonez <japa_jkr@hotmail.com>
2021-07-13 16:46:14 -0300

    Aula 9 Modulo 2

--- src/main/java/net/ufjnet/gestaoobra/services/GestaoProprietario.java
+++ src/main/java/net/ufjnet/gestaoobra/services/GestaoProprietario.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.AllArgsConstructor;
 import net.ufjnet.gestaoobra.models.Proprietario;
 import net.ufjnet.gestaoobra.repositories.ProprietarioDAO;
+import net.ufjnet.gestaoobra.services.exceptions.BusinessException;
 
 @AllArgsConstructor
 @Service
@@ -16,18 +17,38 @@ public class GestaoProprietario {
 	
 	private ProprietarioDAO dao;
 
-	@Transactional
 	public List<Proprietario> findAll() {
 		return dao.findAll();
 	}
 	
-	@Transactional
 	public Optional<Proprietario> findById(Integer id) {
 		return dao.findById(id);
 	}
 	
+	public Optional<Proprietario> findByName(String nome) {
+		return dao.findByNome(nome);
+	}
+	
+	public Optional<Proprietario> findByCpf(String nome) {
+		return dao.findByCpf(nome);
+	}
+	
+	public Optional<Proprietario> findByEmail(String email) {
+		return dao.findByEmail(email);
+	}
+	
 	@Transactional
 	public Proprietario save(Proprietario obj) {
+		boolean cpfExists = dao.findByCpf(obj.getCpf()).stream()
+				.anyMatch(objResult -> !objResult.equals(obj));
+		if(cpfExists) {
+			throw new BusinessException("CPF já cadastrado!");
+		}
+		boolean emailExists = dao.findByEmail(obj.getEmail()).stream()
+				.anyMatch(objResult -> !objResult.equals(obj));
+		if(emailExists) {
+			throw new BusinessException("E-mail já cadastrado!");
+		}
 		return dao.save(obj);
 	}
 	
