65db186fedb34016649f56541d75696a60320b0e
Japaonez <japa_jkr@hotmail.com>
2021-07-14 17:06:06 -0300

    Aula 13 Modulo 1

---
+++ src/main/java/net/ufjnet/gestaoobra/models/Obra.java
@@ -0,0 +1,50 @@
+package net.ufjnet.gestaoobra.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Obras")
+public class Obra implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Integer codigo;
+	
+	@NotBlank
+	@Size(max = 255)
+	@Column(name = "descricao", nullable = false)
+	private String descricao;
+	
+	@NotBlank
+	@Size(max = 255)
+	@Column(nullable = false)
+	private String localizacao;
+	
+	@Column
+	private String complemento;
+	
+//	private Proprietario proprietario;
+}
