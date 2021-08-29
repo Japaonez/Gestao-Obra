8351cdb416b8c3d5917b53e8ad5962efb1c0608c
Jhonata Kumaki Rodrigues <40771665+Japaonez@users.noreply.github.com>
2021-08-29 05:09:28 -0300

    Aula 18 Modulo 1
    Povoando Usuario e Permissao

--- src/main/java/net/ufjnet/gestaoobra/models/Permission.java
+++ src/main/java/net/ufjnet/gestaoobra/models/Permission.java
@@ -1,11 +1,14 @@
 package net.ufjnet.gestaoobra.models;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -32,5 +35,8 @@ public class Permission implements GrantedAuthority, Serializable{
 	public String getAuthority() {
 		return this.descricao;
 	}
+	
+	@ManyToMany(mappedBy = "permission")
+	private List<User> user = new ArrayList<>();
 
 }
