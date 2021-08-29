96ab7983303f84776314497f95c07cabe9165b81
Jhonata Kumaki Rodrigues <40771665+Japaonez@users.noreply.github.com>
2021-08-29 13:12:19 -0300

    Aula 18 Modulo 3
    Não funciona a partir daqui

--- src/main/java/net/ufjnet/gestaoobra/config/SecurityConfig.java
+++ src/main/java/net/ufjnet/gestaoobra/config/SecurityConfig.java
@@ -42,12 +42,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.csrf().disable()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
-				.authorizeRequests()
-				.antMatchers("/autentica/assinatura", "/api-docs/**", "/swagger-ui.html**").permitAll()
-				.antMatchers("/v1/gto/**").authenticated()
-				.antMatchers("/users").denyAll();
-//			.and()
-//			.apply(new JwtTokenConfigure(tokenProvider));
+			.authorizeRequests()
+			.antMatchers("/autentica/assinatura", "/v3/api-docs/**", "/swagger-ui.html**").permitAll()
+			.antMatchers("/v1/gto/**").authenticated()
+			.antMatchers("/users").denyAll()
+			.and()
+			.apply(new JwtTokenConfigure(tokenProvider));
 	}
 
 	@Bean
