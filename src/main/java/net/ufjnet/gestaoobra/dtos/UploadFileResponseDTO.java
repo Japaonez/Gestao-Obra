81772e4d98a55f6d35bec7d34a4a3d81d818aa73
Jhonata Kumaki Rodrigues <40771665+Japaonez@users.noreply.github.com>
2021-09-08 00:08:00 -0300

    Aula 19 Modulo 2
    Ainda não funcional. Error "url"

--- src/main/java/net/ufjnet/gestaoobra/dtos/UploadFileResponseDTO.java
+++ src/main/java/net/ufjnet/gestaoobra/dtos/UploadFileResponseDTO.java
@@ -2,20 +2,11 @@ package net.ufjnet.gestaoobra.dtos;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
-import org.springframework.hateoas.RepresentationModel;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import net.ufjnet.gestaoobra.models.Item;
 
 @Setter
 @Getter
@@ -29,6 +20,4 @@ public class UploadFileResponseDTO implements Serializable{
 	private String fileDownloadUri;
 	private String fileType;
 	private long size;
-	
-	
 }
